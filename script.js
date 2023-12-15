f6d4baf44ae3029c35878935031e3dff91c86c27
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2023-12-15 20:38:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
-let year = prompt("Enter the year");
-function check(year){
+function daysOfAYear(year){
 	if(year % 4 = 0 && year % 100!= 0 || year%400 == 0){
 		return 366;
 	}
 	return 365;
 }
-console.log(check(year));
+let year = prompt("Enter the year");
+alert(daysOfAYear(year));

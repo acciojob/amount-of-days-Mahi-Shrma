5c6d43c82157a3a3d8e3bacdcc80dc2434b515d4
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2023-12-15 20:43:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 //your JS code here. If required.
-function daysOfAYear(year){
-	if(year % 4 = 0 && year % 100!= 0 || year%400 == 0){
-		return 366;
-	}
-	return 365;
+// function daysOfAYear(year){
+// 	if(year % 4 = 0 && year % 100!= 0 || year%400 == 0){
+// 		return 366;
+// 	}
+// 	return 365;
+// }
+// let year = prompt("Enter the year");
+// alert(daysOfAYear(year));
+// A function that takes a year as an input and returns the number of days in that year
+function daysOfAYear(year) {
+  // A leap year is divisible by 4, except if it is also divisible by 100 and not divisible by 400
+  let isLeapYear = (year % 4 == 0) && !(year % 100 == 0 && year % 400 != 0);
+  // A normal year has 365 days, a leap year has 366 days
+  return isLeapYear ? 366 : 365;
 }
-let year = prompt("Enter the year");
-alert(daysOfAYear(year));

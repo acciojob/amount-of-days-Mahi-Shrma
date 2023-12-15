2d3de383a1e050082ee8c638e6d2185f9ca083f6
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2023-12-15 20:27:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let year = prompt("Enter the year");
+function check(year){
+	if(year % 4 = 0 && year % 100!= 0 || year%400 == 0){
+		return 366;
+	}
+	return 365;
+}
+console.log(check(year));
